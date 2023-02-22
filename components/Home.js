db831ee9dd8120aa021a177e3349b2597d9f0a12
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2023-02-22 17:05:25 +0700

    first

---
+++ components/Home.js
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import stylesGlobal from "./pageStyles";
+
+export default function Home(){
+    return(
+        <View style={stylesGlobal.container} ></View>
+    );
+}
---
+++ components/pageStyles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from "react-native";
+
+const stylesGlobal = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ebeced',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  export default stylesGlobal;
